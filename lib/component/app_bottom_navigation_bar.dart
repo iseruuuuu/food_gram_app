7b0be8978af26513ec27f8dc3526fb9b5f6c71e5
iseruuuuu <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>
2023-11-10 09:47:32 +0900

    add: AppBottomNavigationBarを作成しました

---
+++ lib/component/app_bottom_navigation_bar.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class AppBottomNavigationBar extends StatelessWidget {
+  const AppBottomNavigationBar({
+    Key? key,
+    required this.onTap,
+    required this.currentIndex,
+  }) : super(key: key);
+
+  final int currentIndex;
+  final Function(int) onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      decoration: const BoxDecoration(
+        border: Border(
+          top: BorderSide(
+            width: 2,
+          ),
+        ),
+      ),
+      child: BottomNavigationBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        unselectedItemColor: Colors.grey,
+        iconSize: 30,
+        selectedFontSize: 0,
+        unselectedFontSize: 0,
+        type: BottomNavigationBarType.fixed,
+        currentIndex: currentIndex,
+        onTap: onTap,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Padding(
+              padding: EdgeInsets.only(top: 10),
+              child: Icon(
+                Icons.home,
+                size: 35,
+              ),
+            ),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Padding(
+              padding: EdgeInsets.only(top: 10),
+              child: Icon(
+                Icons.search,
+                size: 35,
+              ),
+            ),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Padding(
+              padding: EdgeInsets.only(top: 10),
+              child: Icon(
+                Icons.account_circle,
+                size: 35,
+              ),
+            ),
+            label: '',
+          ),
+        ],
+      ),
+    );
+  }
+}
