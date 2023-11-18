02a5e2ce41f3d4a383a503316d61465622865375
iseruuuuu <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>
2023-11-18 12:38:19 +0900

    add: setupに必要な記載をしました

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:food_gram_app/screen/authentication/login_screen.dart';
+import 'package:food_gram_app/config/environment.dart';
+import 'package:food_gram_app/screen/authentication/authentication_screen.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: Environment().fileName);
+  await Supabase.initialize(
+    anonKey: Environment().supabaseAnonKey,
+    url: Environment().supabaseUrl,
+    debug: kDebugMode,
+    authCallbackUrlHostname: 'login-callback',
+  );
   runApp(const ProviderScope(child: MyApp()));
 }
 
@@ -17,7 +29,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LoginScreen(),
+      home: const AuthenticationScreen(),
     );
   }
 }
