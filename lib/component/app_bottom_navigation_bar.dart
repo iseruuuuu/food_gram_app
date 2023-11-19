81680d15fe9b51eb28abc0e28066127fcb3adec9
iseruuuuu <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>
2023-11-19 10:15:10 +0900

    fix: BottomNavigatorの不具合を修正

--- lib/component/app_bottom_navigation_bar.dart
+++ lib/component/app_bottom_navigation_bar.dart
@@ -12,58 +12,43 @@ class AppBottomNavigationBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      decoration: const BoxDecoration(
-        border: Border(
-          top: BorderSide(
-            width: 2,
+    return BottomNavigationBar(
+      elevation: 0,
+      backgroundColor: Colors.white,
+      unselectedItemColor: Colors.grey,
+      selectedItemColor: Colors.black,
+      selectedLabelStyle: TextStyle(
+        color: Colors.black,
+      ),
+      iconSize: 30,
+      selectedFontSize: 0,
+      unselectedFontSize: 0,
+      type: BottomNavigationBarType.fixed,
+      currentIndex: currentIndex,
+      onTap: onTap,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.home,
+            size: 35,
           ),
+          label: 'home',
         ),
-      ),
-      child: BottomNavigationBar(
-        elevation: 0,
-        backgroundColor: Colors.white,
-        unselectedItemColor: Colors.grey,
-        iconSize: 30,
-        selectedFontSize: 0,
-        unselectedFontSize: 0,
-        type: BottomNavigationBarType.fixed,
-        currentIndex: currentIndex,
-        onTap: onTap,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Padding(
-              padding: EdgeInsets.only(top: 10),
-              child: Icon(
-                Icons.home,
-                size: 35,
-              ),
-            ),
-            label: '',
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.account_circle,
+            size: 35,
           ),
-          BottomNavigationBarItem(
-            icon: Padding(
-              padding: EdgeInsets.only(top: 10),
-              child: Icon(
-                Icons.account_circle,
-                size: 35,
-              ),
-            ),
-            label: '',
+          label: 'profile',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(
+            Icons.settings,
+            size: 35,
           ),
-          BottomNavigationBarItem(
-            icon: Padding(
-              padding: EdgeInsets.only(top: 10),
-              child: Icon(
-                Icons.settings,
-                size: 35,
-              ),
-            ),
-            label: '',
-          ),
-        ],
-      ),
+          label: 'setting',
+        ),
+      ],
     );
   }
 }
