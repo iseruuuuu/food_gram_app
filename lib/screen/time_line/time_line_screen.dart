6d85dd0c3f5bd69e52c3590021e691ac79faa799
iseruuuuu <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>
2023-11-23 15:34:34 +0900

    タイムライン画面のUIを改善しました

--- lib/screen/time_line/time_line_screen.dart
+++ lib/screen/time_line/time_line_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:food_gram_app/component/app_app_bar.dart';
 import 'package:food_gram_app/component/app_floating_button.dart';
 import 'package:food_gram_app/component/app_list_view.dart';
 import 'package:food_gram_app/screen/post/post_screen.dart';
@@ -12,10 +14,8 @@ class TimeLineScreen extends StatelessWidget {
     final stream =
         Supabase.instance.client.from('posts').stream(primaryKey: ['id']);
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-      ),
+      backgroundColor: CupertinoColors.extraLightBackgroundGray,
+      appBar: AppAppBar(),
       body: Column(
         children: [
           AppListView(stream: stream),
