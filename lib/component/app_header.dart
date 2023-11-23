6efe50e9fdff0a962ba2b3dc71c91073c928dc46
iseruuuuu <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>
2023-11-24 08:52:44 +0900

    add:　共通で使えるheaderを作成

---
+++ lib/component/app_header.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class AppHeader extends StatelessWidget {
+  const AppHeader({
+    required this.image,
+    required this.length,
+    required this.name,
+    required this.userName,
+    required this.selfIntroduce,
+    super.key,
+  });
+
+  final String image;
+  final int length;
+  final String name;
+  final String userName;
+  final String selfIntroduce;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              CircleAvatar(
+                backgroundColor: Colors.white,
+                backgroundImage: AssetImage(image),
+                radius: 50,
+              ),
+              Spacer(),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    '$length',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    '投稿',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(width: 10),
+            ],
+          ),
+          SizedBox(height: 10),
+          Text(
+            name,
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            '@$userName',
+            style: TextStyle(
+              fontSize: 18,
+            ),
+          ),
+          SizedBox(height: 5),
+          Text(
+            selfIntroduce,
+            style: TextStyle(
+              fontSize: 16,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
