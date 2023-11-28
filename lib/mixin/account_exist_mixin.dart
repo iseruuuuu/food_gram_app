b155e6f9d94741efba8bf08eeb9a262756ad6922
iseruuuuu <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>
2023-11-28 22:27:08 +0900

    add: ログアウト機能を実装しました

---
+++ lib/mixin/account_exist_mixin.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+mixin AccountExistMixin {
+  final supabase = Supabase.instance.client;
+
+  Future<bool> doesAccountExist() async {
+    final user = supabase.auth.currentUser;
+    if (user == null) {
+      return false;
+    }
+    final response =
+        await supabase.from('users').select().eq('user_id', user.id).execute();
+    print(response.data);
+    if (response.data != null && response.data.isNotEmpty) {
+      return true;
+    }
+    return false;
+  }
+
+  Future<bool> loginStatus() async {
+    final completer = Completer<bool>();
+    Supabase.instance.client.auth.onAuthStateChange.listen((state) {
+      if (state.event == AuthChangeEvent.signedIn) {
+        completer.complete(true);
+      } else {
+        completer.complete(false);
+      }
+    });
+    return completer.future;
+  }
+}
