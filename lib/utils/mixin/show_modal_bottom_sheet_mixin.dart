e9cbb812e08e3ac4735886f0502e76800a52f05a
iseruuuuu <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>
2023-12-17 15:43:27 +0900

    fix: BottomSheetの修正をしました

--- lib/utils/mixin/show_modal_bottom_sheet_mixin.dart
+++ lib/utils/mixin/show_modal_bottom_sheet_mixin.dart
@@ -12,7 +12,7 @@ mixin ShowModalBottomSheetMixin {
       context: context,
       builder: (context) {
         return Container(
-          height: MediaQuery.of(context).size.height / 4,
+          height: MediaQuery.of(context).size.height / 3,
           child: DecoratedBox(
             decoration: BoxDecoration(
               color: Colors.white,
@@ -25,13 +25,34 @@ mixin ShowModalBottomSheetMixin {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Center(
+                  child: SizedBox(
+                    width: MediaQuery.sizeOf(context).width,
+                    child: TextButton(
+                      onPressed: () {
+                        context.pop();
+                        camera();
+                      },
+                      child: Text(
+                        'カメラ',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Divider(),
+                SizedBox(
+                  width: MediaQuery.sizeOf(context).width,
                   child: TextButton(
                     onPressed: () {
                       context.pop();
-                      camera();
+                      album();
                     },
                     child: Text(
-                      'カメラ',
+                      'アルバム',
                       style: TextStyle(
                         fontWeight: FontWeight.bold,
                         fontSize: 16,
@@ -41,29 +62,17 @@ mixin ShowModalBottomSheetMixin {
                   ),
                 ),
                 Divider(),
-                TextButton(
-                  onPressed: () {
-                    context.pop();
-                    album();
-                  },
-                  child: Text(
-                    'アルバム',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                      color: Colors.blue,
-                    ),
-                  ),
-                ),
-                Divider(),
-                TextButton(
-                  onPressed: () => context.pop(),
-                  child: Text(
-                    '閉じる',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                      color: Colors.red,
+                SizedBox(
+                  width: MediaQuery.sizeOf(context).width,
+                  child: TextButton(
+                    onPressed: () => context.pop(),
+                    child: Text(
+                      '閉じる',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                        color: Colors.red,
+                      ),
                     ),
                   ),
                 ),
