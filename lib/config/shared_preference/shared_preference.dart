04da29e5ea0f6df2eae61a580d0b76e82524eb1b
iseruuuuu <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>
2024-01-01 11:35:53 +0900

    add:　チュートリアル画面の表示の有無のためのPreferenceを追加しました

--- lib/config/shared_preference/shared_preference.dart
+++ lib/config/shared_preference/shared_preference.dart
@@ -3,6 +3,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 enum PreferenceKey {
   blockList,
+  isFinishedTutorial,
 }
 
 class Preference {
@@ -16,6 +17,17 @@ class Preference {
   Future<List<String>> getStringList(PreferenceKey key) async {
     final pref = await preference;
     final value = pref.getStringList(EnumToString.convertToString(key)) ?? [];
+    return value;
+  }
+
+  Future<void> setBool(PreferenceKey key) async {
+    final pref = await preference;
+    await pref.setBool(EnumToString.convertToString(key), true);
+  }
+
+  Future<bool> getBool(PreferenceKey key) async {
+    final pref = await preference;
+    final value = pref.getBool(EnumToString.convertToString(key)) ?? false;
     return value;
   }
 }
