0a5a784ff21e8d78391b43e1bb22ed2718432926
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2024-03-23 17:13:24 +0900

    add: レストラン取得の実装しました

--- lib/ui/screen/post/restaurant_screen.dart
+++ lib/ui/screen/post/restaurant_screen.dart
@@ -1,164 +1,120 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:food_gram_app/core/data/api/restaurant_api.dart';
 import 'package:food_gram_app/core/model/restaurant.dart';
-import 'package:food_gram_app/ui/component/app_request.dart';
+import 'package:food_gram_app/ui/component/app_empty.dart';
+import 'package:food_gram_app/ui/component/app_error_widget.dart';
 import 'package:food_gram_app/ui/component/app_text_field.dart';
-import 'package:food_gram_app/ui/screen/post/restaurant_view_model.dart';
-import 'package:food_gram_app/utils/provider/loading.dart';
+import 'package:gap/gap.dart';
 import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 
-class RestaurantScreen extends ConsumerWidget {
+class RestaurantScreen extends HookConsumerWidget {
   const RestaurantScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final controller = ref.watch(restaurantViewModelProvider().notifier);
-    final state = ref.watch(restaurantViewModelProvider());
-    final loading = ref.watch(loadingProvider);
-    return GestureDetector(
-      onTap: () => primaryFocus?.unfocus(),
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          elevation: 1,
-          backgroundColor: Colors.white,
-          surfaceTintColor: Colors.white,
-          leading: GestureDetector(
-            onTap: () {
-              ref.read(restaurantViewModelProvider().notifier).goBack();
-              context.pop();
-            },
-            child: Icon(Icons.close),
-          ),
-        ),
-        body: state.isFirstLoading
-            ? state.isApproval
-                ? Column(
-                    children: [
-                      AppSearchTextField(
-                        controller: controller.controller,
-                        hintText: 'レストランを検索',
-                        onChanged: controller.search,
-                      ),
-                      GestureDetector(
-                        onTap: () {
-                          final restaurant = Restaurant(
-                            restaurant: '自炊',
-                            lat: 0,
-                            lng: 0,
-                          );
-                          primaryFocus?.unfocus();
-                          context.pop(restaurant);
-                        },
-                        child: ListTile(
-                          leading: Icon(Icons.home),
-                          trailing: Icon(
-                            Icons.arrow_forward_ios,
-                            size: 20,
-                          ),
-                          title: Text(
-                            '自炊',
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                      ),
-                      GestureDetector(
-                        onTap: () {
-                          final restaurant = Restaurant(
-                            restaurant: '不明',
-                            lat: 0,
-                            lng: 0,
-                          );
-                          primaryFocus?.unfocus();
-                          context.pop(restaurant);
-                        },
-                        child: ListTile(
-                          leading: Icon(Icons.restaurant_menu),
-                          trailing: Icon(
-                            Icons.arrow_forward_ios,
-                            size: 20,
-                          ),
-                          title: Text(
-                            'レストラン名が不明orヒットしない',
-                            style: TextStyle(
-                              color: Colors.red,
-                              fontSize: 15,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                      ),
-                      if (!loading)
-                        Expanded(
-                          child: ListView.builder(
-                            itemCount: state.restaurant.length,
-                            itemBuilder: (context, index) {
-                              return ListTile(
-                                onTap: () {
-                                  final restaurant = Restaurant(
-                                    restaurant: state.restaurant[index],
-                                    lat: state.lat[index],
-                                    lng: state.log[index],
-                                  );
-                                  primaryFocus?.unfocus();
-                                  context.pop(restaurant);
-                                },
-                                trailing: Icon(
-                                  Icons.arrow_forward_ios,
-                                  size: 20,
-                                ),
-                                title: Text(
-                                  state.restaurant[index],
-                                  style: TextStyle(
-                                    color: Colors.black,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                subtitle: Text(
-                                  state.address[index],
-                                  style: TextStyle(
-                                    color: Colors.grey,
-                                    fontSize: 10,
-                                  ),
-                                ),
-                              );
-                            },
-                          ),
-                        )
-                      else
-                        Expanded(
-                          child: Column(
-                            children: [
-                              Spacer(),
-                              Center(
-                                child: LoadingAnimationWidget.dotsTriangle(
-                                  color: Colors.deepPurple,
-                                  size: 50,
+    final keyword = useState('');
+    final restaurant = ref.watch(restaurantProvider(keyword.value));
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            AppSearchTextField(onChanged: (value) => keyword.value = value),
+            const Gap(20),
+            GestureDetector(
+              onTap: () {
+                final restaurant =
+                    Restaurant(name: '自炊', address: '', lat: 0, lng: 0);
+                primaryFocus?.unfocus();
+                context.pop(restaurant);
+              },
+              child: ListTile(
+                leading: Icon(Icons.home),
+                trailing: Icon(Icons.arrow_forward_ios, size: 20),
+                title: Text(
+                  '自炊',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                final restaurant =
+                    Restaurant(name: '不明', address: '', lat: 0, lng: 0);
+                primaryFocus?.unfocus();
+                context.pop(restaurant);
+              },
+              child: ListTile(
+                leading: Icon(Icons.restaurant_menu),
+                trailing: Icon(Icons.arrow_forward_ios, size: 20),
+                title: Text(
+                  '不明 or ヒットなし',
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            restaurant.when(
+              data: (value) {
+                return Expanded(
+                  child: value.isNotEmpty
+                      ? ListView.builder(
+                          itemCount: value.length,
+                          itemBuilder: (context, index) {
+                            return ListTile(
+                              onTap: () {
+                                final restaurant = Restaurant(
+                                  name: value[index].name,
+                                  address: value[index].address,
+                                  lat: value[index].lat,
+                                  lng: value[index].lng,
+                                );
+                                primaryFocus?.unfocus();
+                                context.pop(restaurant);
+                              },
+                              trailing: Icon(Icons.arrow_forward_ios, size: 20),
+                              title: Text(
+                                value[index].name,
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
                                 ),
                               ),
-                              Spacer(),
-                            ],
-                          ),
-                        ),
-                    ],
-                  )
-                : AppRequest()
-            : Column(
-                children: [
-                  Spacer(),
-                  Center(
-                    child: LoadingAnimationWidget.dotsTriangle(
-                      color: Colors.deepPurple,
-                      size: 50,
-                    ),
+                              subtitle: Text(
+                                value[index].address,
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 11,
+                                ),
+                              ),
+                            );
+                          },
+                        )
+                      : AppSearchEmpty(),
+                );
+              },
+              error: (_, __) {
+                return AppErrorWidget(onTap: () => context.pop());
+              },
+              loading: () => Expanded(
+                child: Center(
+                  child: LoadingAnimationWidget.dotsTriangle(
+                    color: Colors.deepPurple,
+                    size: 50,
                   ),
-                  Spacer(),
-                ],
+                ),
               ),
+            ),
+          ],
+        ),
       ),
     );
   }
