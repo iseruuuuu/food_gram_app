866fa92872c573dfd44b15a5d9a84cb10d059873
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2024-05-19 17:11:44 +0900

    add: Auth画面のUIを修正しました

--- lib/ui/screen/authentication/authentication_screen.dart
+++ lib/ui/screen/authentication/authentication_screen.dart
@@ -4,7 +4,6 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:food_gram_app/core/gen/assets.gen.dart';
-import 'package:food_gram_app/gen/l10n/l10n.dart';
 import 'package:food_gram_app/main.dart';
 import 'package:food_gram_app/router/router.dart';
 import 'package:food_gram_app/ui/component/app_loading.dart';
@@ -14,7 +13,7 @@ import 'package:food_gram_app/utils/mixin/account_exist_mixin.dart';
 import 'package:food_gram_app/utils/provider/loading.dart';
 import 'package:gap/gap.dart';
 import 'package:go_router/go_router.dart';
-import 'package:sign_in_button/sign_in_button.dart';
+import 'package:sign_button/sign_button.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class AuthenticationScreen extends ConsumerStatefulWidget {
@@ -72,6 +71,7 @@ class AuthenticationScreenState extends ConsumerState<AuthenticationScreen>
   Widget build(BuildContext context) {
     final state = ref.watch(authenticationViewModelProvider());
     final loading = ref.watch(loadingProvider);
+    final theme = Theme.of(context);
     return GestureDetector(
       onTap: () => primaryFocus?.unfocus(),
       child: Scaffold(
@@ -87,16 +87,11 @@ class AuthenticationScreenState extends ConsumerState<AuthenticationScreen>
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Assets.image.food.image(
-                        width: 70,
-                        height: 70,
-                      ),
-                      const Text(
+                      Assets.image.food.image(width: 70, height: 70),
+                      Text(
                         'Food Instagram',
-                        style: TextStyle(
-                          fontSize: 30,
+                        style: theme.textTheme.headlineMedium!.copyWith(
                           fontWeight: FontWeight.bold,
-                          color: Colors.black,
                         ),
                       ),
                     ],
@@ -106,69 +101,68 @@ class AuthenticationScreenState extends ConsumerState<AuthenticationScreen>
                     controller: ref
                         .watch(authenticationViewModelProvider().notifier)
                         .emailTextField,
-                    hintText: L10n.of(context).user_id_text_field,
-                    maxLines: 1,
                   ),
                   Gap(20),
                   SizedBox(
-                    width: MediaQuery.of(context).size.width - 100,
-                    height: 50,
-                    child: SignInButton(
-                      Buttons.email,
+                    width: MediaQuery.sizeOf(context).width - 80,
+                    height: 45,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        elevation: 2,
+                        backgroundColor: Colors.black,
+                      ),
                       onPressed: ref
                           .read(authenticationViewModelProvider().notifier)
                           .login,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                    ),
-                  ),
-                  if (Platform.isIOS)
-                    Column(
-                      children: [
-                        Gap(30),
-                        SizedBox(
-                          width: MediaQuery.of(context).size.width - 100,
-                          height: 50,
-                          child: SignInButton(
-                            Buttons.apple,
-                            onPressed: () async {
-                              await ref
-                                  .read(authenticationViewModelProvider()
-                                      .notifier)
-                                  .loginApple();
-                            },
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(15),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.mail,
+                            color: Colors.white,
+                          ),
+                          Gap(10),
+                          Text(
+                            'Sign in with Mail',
+                            style: theme.textTheme.bodyLarge!.copyWith(
+                              color: Colors.white,
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                  Gap(30),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width - 100,
-                    height: 50,
-                    child: SignInButton(
-                      Buttons.google,
-                      onPressed: () async {
-                        await ref
-                            .read(authenticationViewModelProvider().notifier)
-                            .loginGoogle();
-                      },
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15),
+                        ],
                       ),
                     ),
                   ),
-                  Gap(40),
-                  Text(
-                    state.loginStatus,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18,
+                  Gap(20),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 25,
+                      vertical: 10,
                     ),
+                    child: Divider(),
+                  ),
+                  Text(
+                    'SNSログイン',
+                    style: theme.textTheme.titleMedium!.copyWith(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Gap(20),
+                  Wrap(
+                    children: [
+                      if (Platform.isIOS)
+                        SignInButton.mini(
+                          buttonType: ButtonType.appleDark,
+                          onPressed: ref
+                              .read(authenticationViewModelProvider().notifier)
+                              .loginApple,
+                        ),
+                      SignInButton.mini(
+                        buttonType: ButtonType.googleDark,
+                        onPressed: ref
+                            .read(authenticationViewModelProvider().notifier)
+                            .loginGoogle,
+                      ),
+                    ],
                   ),
                 ],
               ),
