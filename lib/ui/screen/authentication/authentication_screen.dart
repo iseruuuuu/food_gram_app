91fef01a5778e46230bbe9645bcab3c6e9a6577b
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2024-05-21 19:00:19 +0900

    fix: リダイレクトの処理をHookConsumerWidgetを使用してリファクタリングしました

--- lib/ui/screen/authentication/authentication_screen.dart
+++ lib/ui/screen/authentication/authentication_screen.dart
@@ -1,7 +1,8 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:food_gram_app/gen/assets.gen.dart';
 import 'package:food_gram_app/main.dart';
@@ -11,65 +12,35 @@ import 'package:food_gram_app/ui/component/app_text_field.dart';
 import 'package:food_gram_app/ui/screen/authentication/authentication_view_model.dart';
 import 'package:food_gram_app/utils/mixin/account_exist_mixin.dart';
 import 'package:food_gram_app/utils/provider/loading.dart';
+import 'package:food_gram_app/utils/snack_bar_manager.dart';
 import 'package:gap/gap.dart';
 import 'package:go_router/go_router.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class AuthenticationScreen extends ConsumerStatefulWidget {
+class AuthenticationScreen extends HookConsumerWidget with AccountExistMixin {
   const AuthenticationScreen({super.key});
 
   @override
-  AuthenticationScreenState createState() => AuthenticationScreenState();
-}
-
-class AuthenticationScreenState extends ConsumerState<AuthenticationScreen>
-    with AccountExistMixin {
-  @override
-  void initState() {
-    init(context);
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    authStateSubscription.cancel();
-    super.dispose();
-  }
-
-  late StreamSubscription<AuthState> authStateSubscription;
-  bool _redirecting = false;
-
-  void init(BuildContext context) {
-    authStateSubscription = supabase.auth.onAuthStateChange.listen((data) {
-      if (!mounted || _redirecting) {
-        return;
-      }
-      final session = data.session;
-      if (session != null) {
-        _redirecting = true;
-        redirect(context);
-      }
-    });
-  }
-
-  Future<void> redirect(BuildContext context) async {
-    if (!await doesAccountExist()) {
-      if (!mounted) {
-        return;
-      }
-      context.pushReplacementNamed(RouterPath.newAccount);
-    } else {
-      if (!mounted) {
-        return;
-      }
-      context.pushReplacementNamed(RouterPath.tab);
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final loading = ref.watch(loadingProvider);
     final theme = Theme.of(context);
+    final controller = ref.watch(authenticationViewModelProvider().notifier);
+    final authStateSubscription = useMemoized(
+      () => supabase.auth.onAuthStateChange.listen((data) {
+        final session = data.session;
+        if (session != null) {
+          redirect(context, ref);
+        }
+      }),
+    );
+
+    useEffect(
+      () {
+        return authStateSubscription.cancel;
+      },
+      [authStateSubscription],
+    );
+
     return GestureDetector(
       onTap: () => primaryFocus?.unfocus(),
       child: Scaffold(
@@ -184,5 +155,14 @@ class AuthenticationScreenState extends ConsumerState<AuthenticationScreen>
         ),
       ),
     );
+  }
+
+  Future<void> redirect(BuildContext context, WidgetRef ref) async {
+    ScaffoldMessenger.of(context).hideCurrentSnackBar();
+    if (!await doesAccountExist()) {
+      context.pushReplacementNamed(RouterPath.tab);
+    } else {
+      context.pushReplacementNamed(RouterPath.newAccount);
+    }
   }
 }
