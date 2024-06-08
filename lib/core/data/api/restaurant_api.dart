b1b846e627f1a2598828a06681bcf8250873f00a
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2024-06-08 22:20:10 +0900

    add: レストラン名の取得をProviderから取得できるようにしました

--- lib/core/data/api/restaurant_api.dart
+++ lib/core/data/api/restaurant_api.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:food_gram_app/constants/api_key.dart';
 import 'package:food_gram_app/core/data/api/dio.dart';
 import 'package:food_gram_app/core/model/restaurant.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:food_gram_app/core/utils/location.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:xml/xml.dart' as xml;
@@ -19,7 +19,7 @@ Future<List<Restaurant>> restaurant(
   final dio = ref.watch(dioProvider);
   try {
     final url = 'http://webservice.recruit.co.jp/hotpepper/gourmet/v1/';
-    final currentLocation = await _getCurrentLocation();
+    final currentLocation = ref.read(locationProvider).value;
     if (currentLocation == LatLng(0, 0)) {
       return [];
     }
@@ -27,7 +27,7 @@ Future<List<Restaurant>> restaurant(
       url,
       queryParameters: {
         'key': RecruitApiKey.apikey,
-        'lat': currentLocation.latitude,
+        'lat': currentLocation!.latitude,
         'lng': currentLocation.longitude,
         'keyword': keyword,
         'range': 5,
@@ -51,28 +51,4 @@ Future<List<Restaurant>> restaurant(
     print(error);
     return throw UnimplementedError();
   }
-}
-
-Future<LatLng> _getCurrentLocation() async {
-  bool serviceEnabled;
-  LocationPermission permission;
-  serviceEnabled = await Geolocator.isLocationServiceEnabled();
-  if (!serviceEnabled) {
-    return LatLng(0, 0);
-  }
-  permission = await Geolocator.checkPermission();
-  if (permission == LocationPermission.denied) {
-    permission = await Geolocator.requestPermission();
-    if (permission == LocationPermission.denied) {
-      return LatLng(0, 0);
-    }
-  }
-  if (permission == LocationPermission.whileInUse ||
-      permission == LocationPermission.always) {
-    final position = await Geolocator.getCurrentPosition();
-    final latitude = position.latitude;
-    final longitude = position.longitude;
-    return LatLng(latitude, longitude);
-  }
-  return LatLng(0, 0);
 }
