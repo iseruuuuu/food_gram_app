638ec84561ca218c0e970e68693d76ba00d45040
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2024-06-08 22:18:49 +0900

    add: 現在地の取得のためのProviderを定義しました

---
+++ lib/core/utils/location.dart
@@ -0,0 +1,34 @@
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'location.g.dart';
+
+@Riverpod(keepAlive: true)
+Future<LatLng> location(LocationRef ref) async {
+  return getLocation();
+}
+
+Future<LatLng> getLocation() async {
+  bool serviceEnabled;
+  LocationPermission permission;
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    return LatLng(0, 0);
+  }
+  permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      return LatLng(0, 0);
+    }
+  }
+  if (permission == LocationPermission.whileInUse ||
+      permission == LocationPermission.always) {
+    final position = await Geolocator.getCurrentPosition();
+    final latitude = position.latitude;
+    final longitude = position.longitude;
+    return LatLng(latitude, longitude);
+  }
+  return LatLng(0, 0);
+}
