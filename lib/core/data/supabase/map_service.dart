86854e5fc0e78447d1065cdfca13cce3a3a920a1
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2024-06-08 22:20:34 +0900

    add: Mapの表示のためのmapServiceを定義しました

---
+++ lib/core/data/supabase/map_service.dart
@@ -0,0 +1,17 @@
+import 'package:food_gram_app/core/data/supabase/post_stream.dart';
+import 'package:food_gram_app/core/model/posts.dart';
+import 'package:food_gram_app/main.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'map_service.g.dart';
+
+@riverpod
+Future<List<Posts>> mapService(MapServiceRef ref) async {
+  final blockList = ref.watch(blockListProvider).asData?.value ?? [];
+  final response = await supabase.from('posts').select().order('created_at');
+  final data = response;
+  return data
+      .map(Posts.fromJson)
+      .where((post) => !blockList.contains(post.userId))
+      .toList();
+}
