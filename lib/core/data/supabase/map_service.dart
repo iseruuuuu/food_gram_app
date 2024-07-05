b83faf78f4ba426bf61401f733bea25bedb37eac
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2024-07-05 10:06:24 +0900

    add: ピンを押したレストランの情報を取得するProviderを作成しました

--- lib/core/data/supabase/map_service.dart
+++ lib/core/data/supabase/map_service.dart
@@ -1,6 +1,7 @@
 import 'package:food_gram_app/core/data/supabase/post_stream.dart';
 import 'package:food_gram_app/core/model/posts.dart';
 import 'package:food_gram_app/main.dart';
+import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'map_service.g.dart';
@@ -15,4 +16,26 @@ Future<List<Posts>> mapService(MapServiceRef ref) async {
       .where((post) => !blockList.contains(post.userId))
       .where((post) => post.lat != 0.0 && post.lng != 0)
       .toList();
+}
+
+@riverpod
+Future<Posts> getRestaurant(
+  GetRestaurantRef ref, {
+  required Point point,
+}) async {
+  final blockList = ref.watch(blockListProvider).asData?.value ?? [];
+  final response = await supabase
+      .from('posts')
+      .select()
+      .eq('lat', point.coordinates.lat)
+      .eq('lng', point.coordinates.lng)
+      .order('created_at');
+  final data = response;
+  final result = data
+      .map((json) {
+        return Posts.fromJson(json);
+      })
+      .where((post) => !blockList.contains(post.userId))
+      .toList()[0];
+  return result;
 }
