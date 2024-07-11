7ef1837f33e20962a3c40d2aac24ef5529145ed9
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2024-07-11 14:38:02 +0900

    add: マップ画面のmodal Sheetのリファクタリング

--- lib/ui/component/modal_sheet/app_modal_sheet.dart
+++ lib/ui/component/modal_sheet/app_modal_sheet.dart
@@ -2,9 +2,10 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:food_gram_app/core/model/posts.dart';
 import 'package:food_gram_app/main.dart';
+import 'package:go_router/go_router.dart';
 
-class RestaurantInfoModal extends StatelessWidget {
-  const RestaurantInfoModal({
+class RestaurantInfoModalSheet extends StatelessWidget {
+  const RestaurantInfoModalSheet({
     required this.post,
     super.key,
   });
