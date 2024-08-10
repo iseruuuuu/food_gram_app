b5f420c9cba754cd791db8968a2bd21a0de856a0
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2024-08-10 11:05:10 +0900

    add: Map画面のModalのUIをリファクタリングする

--- lib/ui/component/modal_sheet/app_modal_sheet.dart
+++ lib/ui/component/modal_sheet/app_modal_sheet.dart
@@ -8,6 +8,7 @@ import 'package:food_gram_app/core/model/model.dart';
 import 'package:food_gram_app/core/model/posts.dart';
 import 'package:food_gram_app/main.dart';
 import 'package:food_gram_app/router/router.dart';
+import 'package:gap/gap.dart';
 import 'package:go_router/go_router.dart';
 
 class RestaurantInfoModalSheet extends ConsumerWidget {
@@ -21,12 +22,11 @@ class RestaurantInfoModalSheet extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final deviceWidth = MediaQuery.of(context).size.width;
-    final deviceHeight = MediaQuery.of(context).size.height;
     return Padding(
       padding: const EdgeInsets.only(bottom: 8),
       child: SizedBox(
         width: deviceWidth,
-        height: deviceWidth / 1.8,
+        height: deviceWidth / 1.7,
         child: ListView.builder(
           itemCount: post.length,
           scrollDirection: Axis.horizontal,
@@ -62,10 +62,11 @@ class RestaurantInfoModalSheet extends ConsumerWidget {
                   borderRadius: BorderRadius.all(Radius.circular(24)),
                   child: Container(
                     width: deviceWidth / 1.4,
-                    height: deviceHeight,
+                    height: deviceWidth / 1.5,
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
+                        Spacer(),
                         Container(
                           width: deviceWidth,
                           height: deviceWidth / 2.7,
@@ -76,24 +77,39 @@ class RestaurantInfoModalSheet extends ConsumerWidget {
                             fit: BoxFit.cover,
                           ),
                         ),
-                        Spacer(),
-                        Material(
+                        Container(
                           color: Colors.white,
-                          child: ListTile(
-                            title: Text(post[index]!.restaurant),
-                            titleTextStyle: TextStyle(
-                              color: Colors.black,
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                            subtitle: Text(post[index]!.foodName),
-                            subtitleTextStyle: TextStyle(
-                              fontSize: 12,
-                              color: Colors.grey.shade600,
+                          width: double.infinity,
+                          height: 80,
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 10),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                FittedBox(
+                                  child: Text(
+                                    post[index]!.restaurant,
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                                Gap(5),
+                                FittedBox(
+                                  child: Text(
+                                    post[index]!.foodName,
+                                    style: TextStyle(
+                                      color: Colors.grey.shade600,
+                                    ),
+                                  ),
+                                ),
+                              ],
                             ),
                           ),
                         ),
-                        Spacer(),
                       ],
                     ),
                   ),
