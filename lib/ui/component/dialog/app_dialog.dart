1687a1e2f03f11ec5301582dda9e5a83222f4826
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2024-08-21 21:15:24 +0900

    add: クレジットのためのダイアログを追加

--- lib/ui/component/dialog/app_dialog.dart
+++ lib/ui/component/dialog/app_dialog.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:food_gram_app/gen/l10n/l10n.dart';
+import 'package:gap/gap.dart';
 import 'package:go_router/go_router.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AppDialog extends StatelessWidget {
   const AppDialog({
@@ -96,6 +99,73 @@ class AppDialog extends StatelessWidget {
               ],
             ),
             const SizedBox(height: 24),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AppNormalDialog extends StatelessWidget {
+  const AppNormalDialog({
+    required this.title,
+    super.key,
+  });
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Gap(20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Text(
+                title,
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            Gap(15),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              child: RichText(
+                text: TextSpan(
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 14,
+                  ),
+                  children: [
+                    TextSpan(text: 'Powered by \n'),
+                    TextSpan(
+                      text: ' ホットペッパーグルメ Webサービス',
+                      style: TextStyle(color: Colors.blue),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () async {
+                          await launchUrl(
+                            Uri.parse('https://webservice.recruit.co.jp/'),
+                          );
+                        },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Gap(20),
           ],
         ),
       ),
