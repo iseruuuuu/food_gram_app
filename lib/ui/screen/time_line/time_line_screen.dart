f7555eb0e353df85efa4c0fc64580beceb40e770
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2024-11-10 18:04:17 +0900

    fix: タイムライン画面にレストラン・自炊の２種類のタブを追加しました

--- lib/ui/screen/time_line/time_line_screen.dart
+++ lib/ui/screen/time_line/time_line_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:food_gram_app/core/data/supabase/post_stream.dart';
 import 'package:food_gram_app/gen/assets.gen.dart';
 import 'package:food_gram_app/router/router.dart';
-import 'package:food_gram_app/ui/component/app_app_bar.dart';
 import 'package:food_gram_app/ui/component/app_error_widget.dart';
 import 'package:food_gram_app/ui/component/app_floating_button.dart';
 import 'package:food_gram_app/ui/component/app_list_view.dart';
@@ -14,43 +13,90 @@ class TimeLineScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final state = ref.watch(postStreamProvider);
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppAppBar(),
-      body: state.when(
-        data: (data) {
-          return AppListView(
-            data: data,
-            routerPath: RouterPath.timeLineDetailPost,
-            refresh: () => ref
-              ..invalidate(postStreamProvider)
-              ..invalidate(blockListProvider),
-          );
-        },
-        error: (_, __) {
-          return AppErrorWidget(
-            onTap: () => ref.refresh(postStreamProvider),
-          );
-        },
-        loading: () {
-          return Center(
-            child: Assets.image.loading.image(
-              fit: BoxFit.cover,
-              width: 100,
-              height: 100,
+    final restaurant = ref.watch(postStreamProvider);
+    final homeMade = ref.watch(postHomeMadeStreamProvider);
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(55),
+          child: AppBar(
+            surfaceTintColor: Colors.transparent,
+            forceMaterialTransparency: true,
+            elevation: 0,
+            bottom: const TabBar(
+              indicatorWeight: 1,
+              automaticIndicatorColorAdjustment: false,
+              labelStyle: TextStyle(fontSize: 12),
+              unselectedLabelColor: Colors.grey,
+              labelColor: Colors.black,
+              indicatorColor: Colors.black,
+              indicatorSize: TabBarIndicatorSize.tab,
+              tabs: <Widget>[
+                Tab(
+                  icon: Icon(Icons.dinner_dining),
+                  text: 'レストラン',
+                  height: 50,
+                ),
+                Tab(
+                  icon: Icon(Icons.restaurant_menu),
+                  text: '自炊',
+                  height: 50,
+                ),
+              ],
             ),
-          );
-        },
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            FoodListView(state: restaurant),
+            FoodListView(state: homeMade),
+          ],
+        ),
+        floatingActionButton: AppFloatingButton(
+          onTap: () async {
+            await context
+                .pushNamed(RouterPath.timeLinePost)
+                .then((value) async {
+              if (value != null) {
+                ref.invalidate(postStreamProvider);
+              }
+            });
+          },
+        ),
       ),
-      floatingActionButton: AppFloatingButton(
-        onTap: () async {
-          await context.pushNamed(RouterPath.timeLinePost).then((value) async {
-            if (value != null) {
-              ref.invalidate(postStreamProvider);
-            }
-          });
-        },
+    );
+  }
+}
+
+class FoodListView extends ConsumerWidget {
+  const FoodListView({
+    required this.state,
+    super.key,
+  });
+
+  final AsyncValue<List<Map<String, dynamic>>> state;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return state.when(
+      data: (data) => AppListView(
+        data: data,
+        routerPath: RouterPath.timeLineDetailPost,
+        refresh: () => ref
+          ..invalidate(postStreamProvider)
+          ..invalidate(blockListProvider),
+      ),
+      error: (_, __) => AppErrorWidget(
+        onTap: () => ref.refresh(postStreamProvider),
+      ),
+      loading: () => Center(
+        child: Assets.image.loading.image(
+          fit: BoxFit.cover,
+          width: 100,
+          height: 100,
+        ),
       ),
     );
   }
