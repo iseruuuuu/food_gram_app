d014923d1dd2e2a643e81150f35a932e38dd4299
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2024-11-23 15:27:22 +0900

    add: 投稿のシェアに画像をシェアに必要なWidgetを作成

---
+++ lib/ui/component/app_share_widget.dart
@@ -0,0 +1,88 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:food_gram_app/core/model/posts.dart';
+import 'package:food_gram_app/core/model/users.dart';
+import 'package:food_gram_app/main.dart';
+import 'package:gap/gap.dart';
+
+class AppShareWidget extends StatelessWidget {
+  const AppShareWidget({
+    required this.posts,
+    required this.users,
+    super.key,
+  });
+
+  final Posts posts;
+  final Users users;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 350,
+      height: 420,
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              width: 350,
+              height: 300,
+              child: ClipRRect(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                ),
+                child: CachedNetworkImage(
+                  imageUrl: supabase.storage
+                      .from('food')
+                      .getPublicUrl(posts.foodImage),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Gap(10),
+                  Text(
+                    posts.foodName,
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  Gap(10),
+                  Text(
+                    'IN ${posts.restaurant}',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  Row(
+                    children: [
+                      Spacer(),
+                      Text('#FoodGram'),
+                    ],
+                  ),
+                  Gap(10),
+                ],
+              ),
+            ),
+          ],
+        ),
+        color: Colors.white,
+        elevation: 0,
+      ),
+    );
+  }
+}
