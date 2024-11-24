e2867aea6b865fc904accc37eb4a76d1c23f498c
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2024-11-24 21:12:25 +0900

    add: 投稿画面のカテゴリーのWidgetを作成

---
+++ lib/ui/screen/post/component/post_category_widget.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:food_gram_app/ui/component/modal_sheet/app_post_category_modal_sheet.dart';
+
+class PostCategoryWidget extends StatelessWidget {
+  const PostCategoryWidget({
+    required this.tag,
+    required this.category,
+    required this.title,
+    required this.modalSheetHeight,
+    super.key,
+  });
+
+  final ValueNotifier<String> tag;
+  final List<String> category;
+  final String title;
+  final double modalSheetHeight;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        showModalBottomSheet(
+          context: context,
+          builder: (context) {
+            return AppPostCategoryModalSheet(
+              category: category,
+              onChanged: (value) {
+                tag.value = value;
+              },
+              tagValue: tag.value,
+              height: modalSheetHeight,
+            );
+          },
+        );
+      },
+      child: Container(
+        width: 100,
+        height: 50,
+        decoration: BoxDecoration(
+          border: Border.all(),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Center(
+          child: Text(
+            tag.value.isNotEmpty ? tag.value : title,
+            style: TextStyle(
+              fontSize: tag.value.isNotEmpty ? 30 : 18,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
