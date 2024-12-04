3c7a6f0eb76d10ebe68198e233b330103cf63505
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2024-12-04 10:06:10 +0900

    add: appListViewのリファクタリングをしました

--- lib/ui/component/app_list_view.dart
+++ lib/ui/component/app_list_view.dart
@@ -19,11 +19,12 @@ class AppListView extends ConsumerWidget {
 
   final List<Map<String, dynamic>> data;
   final String routerPath;
-  final Function() refresh;
+  final VoidCallback refresh;
   final bool isTimeLine;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final screenWidth = MediaQuery.of(context).size.width / 3;
     return data.isNotEmpty
         ? RefreshIndicator(
             color: Colors.black,
@@ -40,14 +41,16 @@ class AppListView extends ConsumerWidget {
                       height: 100,
                       child: StoryWidget(data: data),
                     ),
-                  )
-                else
-                  SliverToBoxAdapter(child: SizedBox()),
+                  ),
                 SliverGrid(
                   delegate: SliverChildBuilderDelegate(
                     (context, index) {
+                      final foodImageUrl = supabase.storage
+                          .from('food')
+                          .getPublicUrl(data[index]['food_image']);
+
                       return GestureDetector(
-                        onTap: () async {
+                        onTap: () {
                           EasyDebounce.debounce(
                             'click_detail',
                             const Duration(milliseconds: 300),
@@ -55,16 +58,13 @@ class AppListView extends ConsumerWidget {
                               final post = await ref
                                   .read(postsServiceProvider)
                                   .getPost(data, index);
-                              await context
-                                  .pushNamed(
+                              final result = await context.pushNamed(
                                 routerPath,
                                 extra: post,
-                              )
-                                  .then((value) {
-                                if (value != null) {
-                                  refresh();
-                                }
-                              });
+                              );
+                              if (result != null) {
+                                refresh();
+                              }
                             },
                           );
                         },
@@ -72,16 +72,11 @@ class AppListView extends ConsumerWidget {
                           elevation: 10,
                           child: ClipRRect(
                             borderRadius: BorderRadius.circular(10),
-                            child: Container(
-                              width: MediaQuery.of(context).size.width / 3,
-                              height: MediaQuery.of(context).size.width / 3,
-                              color: Colors.white,
-                              child: CachedNetworkImage(
-                                imageUrl: supabase.storage
-                                    .from('food')
-                                    .getPublicUrl(data[index]['food_image']),
-                                fit: BoxFit.cover,
-                              ),
+                            child: CachedNetworkImage(
+                              imageUrl: foodImageUrl,
+                              fit: BoxFit.cover,
+                              width: screenWidth,
+                              height: screenWidth,
                             ),
                           ),
                         ),
@@ -98,6 +93,6 @@ class AppListView extends ConsumerWidget {
               ],
             ),
           )
-        : AppEmpty();
+        : const AppEmpty();
   }
 }
