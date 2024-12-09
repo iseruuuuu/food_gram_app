66bc95f110bb2fc0c80b339d18cdff901f274480
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2024-12-09 18:56:26 +0900

    add: サブスクをしている状態かどうかのProviderを作成

---
+++ lib/core/data/purchase/subscription_provider.dart
@@ -0,0 +1,27 @@
+import 'package:purchases_flutter/purchases_flutter.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'subscription_provider.g.dart';
+
+@riverpod
+Future<bool> subscription(
+  SubscriptionRef ref,
+) async {
+  try {
+    // RevenueCatからCustomerInfoを取得
+    final customerInfo = await Purchases.getCustomerInfo();
+
+    // 指定されたエンタイトルメントがアクティブか確認
+    final entitlements = customerInfo.entitlements.all;
+    if (entitlements.containsKey('foodgram_premium_membership') &&
+        entitlements['foodgram_premium_membership']!.isActive) {
+      print('サブスクできてる！！');
+
+      return true;
+    }
+    return false;
+  } catch (e) {
+    print('Error checking entitlement: $e');
+    return false; // エラー発生時はfalseを返す
+  }
+}
