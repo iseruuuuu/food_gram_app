4fb338e9efa2553691e3285a657acf628537638b
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2024-12-10 14:57:32 +0900

    add: 設定画面でサブスクの購入の処理と復元の処理を記載

--- lib/ui/screen/setting/setting_view_model.dart
+++ lib/ui/screen/setting/setting_view_model.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:battery_info/battery_info_plugin.dart';
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
+import 'package:food_gram_app/core/data/purchase/purchase_provider.dart';
 import 'package:food_gram_app/core/data/supabase/auth/auth_service.dart';
 import 'package:food_gram_app/gen/l10n/l10n.dart';
 import 'package:food_gram_app/ui/screen/setting/setting_state.dart';
@@ -87,6 +88,20 @@ class SettingViewModel extends _$SettingViewModel {
         ),
       );
     }
+  }
+
+  Future<void> purchase() async {
+    loading.state = true;
+    await ref.read(purchaseProvider.notifier).makePurchase('FoodGramのメンバーシップ');
+    loading.state = false;
+  }
+
+  Future<void> restore() async {
+    loading.state = true;
+    await ref
+        .read(purchaseProvider.notifier)
+        .restorePurchase('foodgram_premium_membership');
+    loading.state = false;
   }
 
   Future<bool> signOut() async {
