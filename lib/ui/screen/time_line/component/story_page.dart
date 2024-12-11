1c30573349ccdf3cd1f75d897db6a6790f1e9c74
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2024-12-11 18:27:03 +0900

    add: ３つの画面にバナー広告を入れてみた

--- lib/ui/screen/time_line/component/story_page.dart
+++ lib/ui/screen/time_line/component/story_page.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:food_gram_app/core/data/admob/admob_banner.dart';
 import 'package:food_gram_app/core/model/posts.dart';
 import 'package:food_gram_app/core/model/users.dart';
 import 'package:food_gram_app/main.dart';
@@ -18,92 +19,95 @@ class StoryPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: Padding(
-        padding: const EdgeInsets.only(top: 10),
-        child: StoryPageView(
-          itemBuilder: (context, pageIndex, storyIndex) {
-            return Stack(
-              children: [
-                Positioned.fill(child: Container(color: Colors.black)),
-                Positioned.fill(
-                  child: CachedNetworkImage(
-                    imageUrl: supabase.storage
-                        .from('food')
-                        .getPublicUrl(posts.foodImage),
-                    fit: BoxFit.fitWidth,
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        body: Padding(
+          padding: const EdgeInsets.only(top: 10),
+          child: StoryPageView(
+            itemBuilder: (context, pageIndex, storyIndex) {
+              return Stack(
+                children: [
+                  Positioned.fill(child: Container(color: Colors.black)),
+                  Positioned.fill(
+                    child: CachedNetworkImage(
+                      imageUrl: supabase.storage
+                          .from('food')
+                          .getPublicUrl(posts.foodImage),
+                      fit: BoxFit.fitWidth,
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 44, left: 8),
-                  child: Row(
-                    children: [
-                      CircleAvatar(
-                        radius: 28,
-                        backgroundImage: AssetImage(users.image),
-                        backgroundColor: Colors.white,
-                      ),
-                      Gap(12),
-                      Text(
-                        users.name,
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
+                  Padding(
+                    padding: const EdgeInsets.only(top: 44, left: 8),
+                    child: Row(
+                      children: [
+                        CircleAvatar(
+                          radius: 28,
+                          backgroundImage: AssetImage(users.image),
+                          backgroundColor: Colors.white,
                         ),
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Spacer(),
-                      Text(
-                        posts.restaurant,
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
+                        Gap(12),
+                        Text(
+                          users.name,
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      ),
-                      Text(
-                        posts.foodTag,
-                        style: TextStyle(fontSize: 40),
-                      ),
-                      Gap(50),
-                    ],
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Spacer(),
+                        Text(
+                          posts.restaurant,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                        ),
+                        Text(
+                          posts.foodTag,
+                          style: TextStyle(fontSize: 40),
+                        ),
+                        Gap(50),
+                        AdmobBanner(),
+                      ],
+                    ),
+                  ),
+                ],
+              );
+            },
+            gestureItemBuilder: (context, pageIndex, storyIndex) {
+              return Align(
+                alignment: Alignment.topRight,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 32),
+                  child: IconButton(
+                    padding: EdgeInsets.zero,
+                    color: Colors.white,
+                    icon: Icon(Icons.close),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
                   ),
                 ),
-              ],
-            );
-          },
-          gestureItemBuilder: (context, pageIndex, storyIndex) {
-            return Align(
-              alignment: Alignment.topRight,
-              child: Padding(
-                padding: const EdgeInsets.only(top: 32),
-                child: IconButton(
-                  padding: EdgeInsets.zero,
-                  color: Colors.white,
-                  icon: Icon(Icons.close),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                ),
-              ),
-            );
-          },
-          pageLength: 1,
-          storyLength: (pageIndex) {
-            return 1;
-          },
-          onPageLimitReached: () {
-            Navigator.pop(context);
-          },
+              );
+            },
+            pageLength: 1,
+            storyLength: (pageIndex) {
+              return 1;
+            },
+            onPageLimitReached: () {
+              Navigator.pop(context);
+            },
+          ),
         ),
       ),
     );
