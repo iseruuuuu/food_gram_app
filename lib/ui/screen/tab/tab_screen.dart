89205d26319200cb6069b299edb62298f51a6cdc
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2024-12-14 16:09:55 +0900

    fix: Androidのためにタブの調整をしました

--- lib/ui/screen/tab/tab_screen.dart
+++ lib/ui/screen/tab/tab_screen.dart
@@ -12,68 +12,47 @@ class TabScreen extends ConsumerWidget {
     final controller = ref.watch(tabViewModelProvider().notifier);
     return Scaffold(
       body: controller.pageList[state.selectedIndex],
-      bottomNavigationBar: SizedBox(
-        height: 80,
-        child: Theme(
-          data: Theme.of(context).copyWith(
-            splashColor: Colors.transparent,
-            highlightColor: Colors.transparent,
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: state.selectedIndex,
+        onTap: controller.onTap,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.fastfood_outlined,
+              semanticLabel: 'timelineIcon',
+            ),
+            label: '',
           ),
-          child: BottomNavigationBar(
-            currentIndex: state.selectedIndex,
-            onTap: controller.onTap,
-            items: <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                icon: Padding(
-                  padding: const EdgeInsets.only(top: 18),
-                  child: Icon(
-                    Icons.fastfood_outlined,
-                    semanticLabel: 'timelineIcon',
-                  ),
-                ),
-                label: '',
-              ),
-              BottomNavigationBarItem(
-                icon: Padding(
-                  padding: const EdgeInsets.only(top: 18),
-                  child: Icon(
-                    CupertinoIcons.map,
-                    semanticLabel: 'mapIcon',
-                  ),
-                ),
-                label: '',
-              ),
-              BottomNavigationBarItem(
-                icon: Padding(
-                  padding: const EdgeInsets.only(top: 18),
-                  child: Icon(
-                    CupertinoIcons.profile_circled,
-                    semanticLabel: 'profileIcon',
-                  ),
-                ),
-                label: '',
-              ),
-              BottomNavigationBarItem(
-                icon: Padding(
-                  padding: const EdgeInsets.only(top: 18),
-                  child: Icon(
-                    Icons.settings,
-                    semanticLabel: 'settingIcon',
-                  ),
-                ),
-                label: '',
-              ),
-            ],
-            type: BottomNavigationBarType.fixed,
-            iconSize: 28,
-            elevation: 0,
-            backgroundColor: Colors.white,
-            selectedItemColor: Colors.black,
-            unselectedItemColor: Colors.grey,
-            selectedFontSize: 0,
-            unselectedFontSize: 0,
+          BottomNavigationBarItem(
+            icon: Icon(
+              CupertinoIcons.map,
+              semanticLabel: 'mapIcon',
+            ),
+            label: '',
           ),
-        ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              CupertinoIcons.profile_circled,
+              semanticLabel: 'profileIcon',
+            ),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.settings,
+              semanticLabel: 'settingIcon',
+            ),
+            label: '',
+          ),
+        ],
+        type: BottomNavigationBarType.fixed,
+        iconSize: 28,
+        elevation: 0,
+        backgroundColor: Colors.white,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.grey,
+        selectedFontSize: 0,
+        unselectedFontSize: 0,
       ),
     );
   }
