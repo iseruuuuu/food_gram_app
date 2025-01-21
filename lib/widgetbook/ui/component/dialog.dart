724aa255e65490c6083759877d486b60bf7a190c
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2025-01-21 14:50:34 +0900

    add: 不要な記載を削除

--- lib/widgetbook/ui/component/dialog.dart
+++ lib/widgetbook/ui/component/dialog.dart
@@ -1,30 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:food_gram_app/ui/component/dialog/app_dialog.dart';
-import 'package:food_gram_app/ui/component/dialog/app_logout_dialog.dart';
 import 'package:food_gram_app/ui/component/dialog/app_profile_dialog.dart';
 import 'package:food_gram_app/ui/component/dialog/app_share_dialog.dart';
 import 'package:food_gram_app/widgetbook/ui/fake_data/fake_data.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
-
-@widgetbook.UseCase(
-  name: 'AppDialog',
-  type: AppDialog,
-)
-AppDialog appDialog(BuildContext context) {
-  return AppDialog(
-    onTap: () {},
-    title: 'test title',
-    subTitle: 'test subtitle',
-  );
-}
-
-@widgetbook.UseCase(
-  name: 'AppLogoutDialog',
-  type: AppLogoutDialog,
-)
-AppLogoutDialog appLogoutDialog(BuildContext context) {
-  return AppLogoutDialog(logout: () {});
-}
 
 @widgetbook.UseCase(
   name: 'AppProfileDialog',
