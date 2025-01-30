01be721f8bb7a21b7726c52b6453bf8108bb2f16
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2025-01-30 16:12:54 +0900

    add: google_restaurant_api->google_restauarnt_serviceに変更し、core/api/servicesフォルダに移動しました

--- lib/core/api/restaurant/services/google_restaurant_service.dart
+++ lib/core/api/restaurant/services/google_restaurant_service.dart
@@ -2,44 +2,49 @@ import 'dart:io';
 
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:food_gram_app/core/data/api/dio.dart';
+import 'package:food_gram_app/core/api/client/dio_client.dart';
 import 'package:food_gram_app/core/model/restaurant.dart';
 import 'package:food_gram_app/core/utils/provider/location.dart';
 import 'package:food_gram_app/env.dart';
 import 'package:maplibre_gl/maplibre_gl.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-part 'google_restaurant_api.g.dart';
+part 'google_restaurant_service.g.dart';
 
 typedef PaginationList<T> = List<T>;
 
 @riverpod
-Future<PaginationList<Restaurant>> googleRestaurantApi(
+Future<PaginationList<Restaurant>> googleRestaurantServices(
   Ref ref,
   String keyword,
 ) async {
-  final dio = ref.watch(dioProvider);
+  final dio = ref.watch(dioClientProvider);
   final currentLocationFuture = ref.read(locationProvider.future);
   final currentLocation = await currentLocationFuture;
   if (currentLocation == LatLng(0, 0) || keyword.isEmpty) {
     return [];
   }
   final restaurants = <Restaurant>[];
-  final googleRestaurants = await _search(dio, keyword);
+  final googleRestaurants = await _search(dio, ref, keyword);
   restaurants.addAll(googleRestaurants);
   return restaurants;
 }
 
 Future<List<Restaurant>> _search(
   Dio dio,
+  Ref ref,
   String keyword,
 ) async {
+  final currentLocationFuture = ref.read(locationProvider.future);
+  final currentLocation = await currentLocationFuture;
+
   final response = await dio.get(
     'https://maps.googleapis.com/maps/api/place/textsearch/json',
     queryParameters: {
       'query': keyword,
       'key': Platform.isIOS ? Env.iOSGoogleApikey : Env.androidGoogleApikey,
       'language': 'ja',
+      'location': '${currentLocation.latitude},${currentLocation.longitude}',
     },
   );
   if (response.statusCode == 200) {
