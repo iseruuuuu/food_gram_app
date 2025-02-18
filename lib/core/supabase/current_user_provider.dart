1209c08384c55f8146881f9d9398121d7e60d4a8
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2025-02-18 14:40:19 +0900

    add: currentUserのためのProviderを作成しました

---
+++ lib/core/supabase/current_user_provider.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+part 'current_user_provider.g.dart';
+
+@Riverpod(keepAlive: true)
+SupabaseClient supabase(Ref ref) {
+  return Supabase.instance.client;
+}
+
+@Riverpod(keepAlive: true) // keepAlive: true でプロバイダーを永続化
+class CurrentUser extends _$CurrentUser {
+  @override
+  String? build() {
+    return ref.read(supabaseProvider).auth.currentUser?.id;
+  }
+
+  String? get value => state;
+
+  set value(String? userId) {
+    state = userId;
+  }
+
+  // ログアウト時にクリア
+  void clear() {
+    state = null;
+  }
+}
