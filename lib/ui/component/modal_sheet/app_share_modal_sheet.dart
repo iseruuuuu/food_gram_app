1f2ea6bb1558ec5d37c87250bda9c1a56c9832df
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2025-03-07 10:43:12 +0900

    add: シェアするときに画像のみor画像＋テキストの選択をできるように実装しました

---
+++ lib/ui/component/modal_sheet/app_share_modal_sheet.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:food_gram_app/gen/l10n/l10n.dart';
+import 'package:go_router/go_router.dart';
+
+class AppShareModalSheet extends StatelessWidget {
+  const AppShareModalSheet({
+    required this.sharePost,
+    required this.shareOnlyPost,
+    super.key,
+  });
+
+  final VoidCallback sharePost;
+  final VoidCallback shareOnlyPost;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height / 3.5,
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(30),
+            topLeft: Radius.circular(30),
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: SizedBox(
+                width: MediaQuery.sizeOf(context).width,
+                child: TextButton(
+                  onPressed: () {
+                    context.pop();
+                    sharePost();
+                  },
+                  child: Text(
+                    L10n.of(context).shareTextAndImage,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                      color: Colors.blue,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Divider(),
+            SizedBox(
+              width: MediaQuery.sizeOf(context).width,
+              child: TextButton(
+                onPressed: () {
+                  context.pop();
+                  shareOnlyPost();
+                },
+                child: Text(
+                  L10n.of(context).shareImageOnly,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                    color: Colors.blue,
+                  ),
+                ),
+              ),
+            ),
+            Divider(),
+            SizedBox(
+              width: MediaQuery.sizeOf(context).width,
+              child: TextButton(
+                onPressed: () => context.pop(),
+                child: Text(
+                  L10n.of(context).close,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                    color: Colors.red,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
