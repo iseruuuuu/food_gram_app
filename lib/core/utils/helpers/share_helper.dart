05b27fe818238072719319d877d716689f4e4d04
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2025-03-07 10:44:58 +0900

    add: シェアの内容をHelperにまとめました

--- lib/core/utils/helpers/share_helper.dart
+++ lib/core/utils/helpers/share_helper.dart
@@ -1,3 +1,12 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:food_gram_app/core/model/posts.dart';
+import 'package:food_gram_app/ui/component/app_share_widget.dart';
+import 'package:food_gram_app/ui/component/modal_sheet/app_share_modal_sheet.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:screenshot/screenshot.dart';
 import 'package:share_plus/share_plus.dart';
 
 class ShareHelpers {
@@ -15,7 +24,72 @@ class ShareHelpers {
     );
   }
 
-  Future<void> sharePostsForInstagram(List<XFile> files) async {
+  Future<void> shareOnlyPost(List<XFile> files) async {
     await Share.shareXFiles(files);
+  }
+
+  Future<void> openShareModal({
+    required Posts posts,
+    required WidgetRef ref,
+    required ValueNotifier<bool> loading,
+    required BuildContext context,
+    String? shareText,
+  }) async {
+    await showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return AppShareModalSheet(
+          sharePost: () async {
+            await captureAndShare(
+              widget: AppShareWidget(
+                posts: posts,
+                ref: ref,
+              ),
+              shareText: '${posts.foodName} '
+                  'in ${posts.restaurant}',
+              loading: loading,
+              hasText: true,
+            );
+          },
+          shareOnlyPost: () async {
+            await captureAndShare(
+              widget: AppShareWidget(
+                posts: posts,
+                ref: ref,
+              ),
+              loading: loading,
+              hasText: false,
+            );
+          },
+        );
+      },
+    );
+  }
+
+  Future<void> captureAndShare({
+    required Widget widget,
+    required ValueNotifier<bool> loading,
+    required bool hasText,
+    String? shareText,
+  }) async {
+    try {
+      loading.value = true;
+      final screenshotController = ScreenshotController();
+      final screenshotBytes =
+          await screenshotController.captureFromWidget(widget);
+
+      // 一時ディレクトリに保存
+      final tempDir = await getTemporaryDirectory();
+      final filePath = '${tempDir.path}/shared_image.png';
+      final file = File(filePath);
+      await file.writeAsBytes(screenshotBytes);
+      if (hasText) {
+        await sharePosts([XFile(file.path)], shareText!);
+      } else {
+        await shareOnlyPost([XFile(file.path)]);
+      }
+    } finally {
+      loading.value = false;
+    }
   }
 }
