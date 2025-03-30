439e2e729202c073dcb4f357694a8038e817a94f
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2025-03-30 10:30:18 +0900

    add: EditPostStyle画面で使われているStyleをtheme/styleに移動しました

---
+++ lib/core/theme/style/edit_post_style.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class EditPostStyle {
+  EditPostStyle._();
+
+  static TextStyle editTitle() {
+    return TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.bold,
+      color: Colors.black,
+    );
+  }
+
+  static TextStyle editButton() {
+    return TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.bold,
+      color: Colors.blue,
+    );
+  }
+
+  static TextStyle restaurant() {
+    return TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.bold,
+      color: Colors.black,
+    );
+  }
+
+  static TextStyle category() {
+    return TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.normal,
+      color: Colors.black,
+    );
+  }
+}
