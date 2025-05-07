4f4e6be6459ea24cc8de67548127e723a896f162
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2025-05-07 11:42:53 +0900

    add: 投稿詳細画面の投稿内容を更新できるように常に最新の状態を取得できるようにしてみました

--- lib/ui/screen/detail/detail_post_state.dart
+++ lib/ui/screen/detail/detail_post_state.dart
@@ -1,3 +1,4 @@
+import 'package:food_gram_app/core/model/posts.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'detail_post_state.freezed.dart';
@@ -8,5 +9,17 @@ abstract class DetailPostState with _$DetailPostState {
     @Default('') String status,
     @Default(0) int heart,
     @Default(false) bool isSuccess,
+    @Default(false) bool isLoading,
   }) = _DetailPostState;
+}
+
+@freezed
+abstract class PostState with _$PostState {
+  const factory PostState.loading() = PostStateLoading;
+
+  const factory PostState.data({
+    required Posts posts,
+  }) = _PostStateData;
+
+  const factory PostState.error() = PostStateError;
 }
--- lib/ui/screen/detail/detail_post_view_model.dart
+++ lib/ui/screen/detail/detail_post_view_model.dart
@@ -2,9 +2,11 @@ import 'package:food_gram_app/core/local/shared_preference.dart';
 import 'package:food_gram_app/core/model/posts.dart';
 import 'package:food_gram_app/core/supabase/post/providers/block_list_provider.dart';
 import 'package:food_gram_app/core/supabase/post/providers/post_stream_provider.dart';
+import 'package:food_gram_app/core/supabase/post/repository/post_repository.dart';
 import 'package:food_gram_app/core/supabase/post/services/delete_service.dart';
 import 'package:food_gram_app/core/utils/provider/loading.dart';
 import 'package:food_gram_app/ui/screen/detail/detail_post_state.dart';
+import 'package:logger/logger.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'detail_post_view_model.g.dart';
@@ -48,5 +50,43 @@ class DetailPostViewModel extends _$DetailPostViewModel {
     await Future<void>.delayed(const Duration(seconds: 2));
     loading.state = false;
     return true;
+  }
+}
+
+@riverpod
+class PostsViewModel extends _$PostsViewModel {
+  @override
+  PostState build(int postId) {
+    getData(postId);
+    return const PostState.loading();
+  }
+
+  final logger = Logger();
+
+  Future<void> getData(int postId) async {
+    state = const PostState.loading();
+    try {
+      final result =
+          await ref.read(postRepositoryProvider.notifier).getPost(postId);
+      result.when(
+        success: (posts) {
+          state = PostState.data(posts: posts);
+        },
+        failure: (error) {
+          state = const PostState.error();
+        },
+      );
+    } on Exception catch (error) {
+      logger.e(error);
+      state = const PostState.error();
+    }
+  }
+
+  void setUser(Posts posts) {
+    state.whenOrNull(
+      data: (posts) {
+        state = PostState.data(posts: posts);
+      },
+    );
   }
 }
