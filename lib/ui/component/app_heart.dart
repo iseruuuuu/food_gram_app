d15910fc3b320e62083848441f7ade4f1ee4f585
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2025-05-20 21:40:20 +0900

    add: いいねの演出をLottieを使用していいものに差し替えました

--- lib/ui/component/app_heart.dart
+++ lib/ui/component/app_heart.dart
@@ -1,31 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:food_gram_app/gen/assets.gen.dart';
-import 'package:gif/gif.dart';
+import 'package:lottie/lottie.dart';
 
-class AppHeart extends StatelessWidget {
+class AppHeart extends HookWidget {
   const AppHeart({
     required this.isHeart,
-    required this.controller,
     super.key,
   });
 
   final bool isHeart;
-  final GifController controller;
 
   @override
   Widget build(BuildContext context) {
-    return isHeart
+    final showHeart = useState(false);
+    useEffect(
+      () {
+        if (isHeart) {
+          showHeart.value = true;
+          Future.delayed(const Duration(milliseconds: 1200), () {
+            showHeart.value = false;
+          });
+        }
+        return null;
+      },
+      [isHeart],
+    );
+
+    return showHeart.value
         ? Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Center(
-                  child: Gif(
-                    controller: controller,
-                    autostart: Autostart.once,
-                    image: AssetImage(Assets.image.heart.path),
-                    onFetchCompleted: controller.stop,
-                  ),
+                Lottie.asset(
+                  Assets.lottie.heart,
+                  repeat: false,
+                  width: 600,
+                  height: 600,
                 ),
                 const SizedBox(height: 100),
               ],
