6df1037cf3c72d9694a9cd50bf90aa0eb2206ea2
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2025-08-29 13:31:52 +0900

    add:　１日でいいねできる上限値を🔟回にしてみました

--- lib/core/local/shared_preference.dart
+++ lib/core/local/shared_preference.dart
@@ -6,6 +6,8 @@ enum PreferenceKey {
   isFinishedTutorial,
   isAccept,
   heartList,
+  heartCount,
+  heartDate,
 }
 
 class Preference {
@@ -39,5 +41,48 @@ class Preference {
   Future<bool> getBool(PreferenceKey key) async {
     final pref = await _prefs;
     return pref.getBool(_getKey(key)) ?? false;
+  }
+
+  Future<void> setInt(PreferenceKey key, int value) async {
+    final pref = await _prefs;
+    await pref.setInt(_getKey(key), value);
+  }
+
+  Future<int> getInt(PreferenceKey key) async {
+    final pref = await _prefs;
+    return pref.getInt(_getKey(key)) ?? 0;
+  }
+
+  Future<void> setString(PreferenceKey key, String value) async {
+    final pref = await _prefs;
+    await pref.setString(_getKey(key), value);
+  }
+
+  Future<String> getString(PreferenceKey key) async {
+    final pref = await _prefs;
+    return pref.getString(_getKey(key)) ?? '';
+  }
+
+  /// 同じ日付で10回以上いいねした場合はfalseを返す
+  Future<bool> canLike() async {
+    final today = DateTime.now().toIso8601String().split('T')[0];
+    final storedDate = await getString(PreferenceKey.heartDate);
+    final currentCount = await getInt(PreferenceKey.heartCount);
+
+    // 日付が変わった場合はカウントをリセット
+    if (storedDate != today) {
+      await setString(PreferenceKey.heartDate, today);
+      await setInt(PreferenceKey.heartCount, 0);
+      return true;
+    }
+
+    // 10回以上いいねした場合はfalse
+    return currentCount < 10;
+  }
+
+  /// いいねカウントを増加
+  Future<void> incrementHeartCount() async {
+    final currentCount = await getInt(PreferenceKey.heartCount);
+    await setInt(PreferenceKey.heartCount, currentCount + 1);
   }
 }
