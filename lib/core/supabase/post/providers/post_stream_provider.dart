df389d1ee068681873785f41b9ece7aa54e894f3
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2025-09-13 13:40:49 +0900

    fix: 使用するStreamProviderをカテゴリーごとの投稿（全取得付き）と自分の投稿の２種類だけにしました

--- lib/core/supabase/post/providers/post_stream_provider.dart
+++ lib/core/supabase/post/providers/post_stream_provider.dart
@@ -7,10 +7,7 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'post_stream_provider.g.dart';
 
-/// 投稿のキャッシュを保持するプロバイダー
-final postCacheProvider = StateProvider<List<Posts>>((ref) => []);
-
-/// カテゴリー内の全ての絵文字を対象にフィルタリングするプロバイダー
+/// 全取得・カテゴリーごとの取得のためのStreamProvider
 @riverpod
 Stream<List<Posts>> postsStream(
   Ref ref,
@@ -18,51 +15,26 @@ Stream<List<Posts>> postsStream(
 ) {
   final blockList = ref.watch(blockListProvider).asData?.value ?? [];
   final supabase = ref.read(supabaseProvider);
-  final cachedPosts = ref.watch(postCacheProvider);
-
-  // キャッシュされた投稿がある場合は、それを使用
-  if (cachedPosts.isNotEmpty) {
-    return Stream.value(
-      cachedPosts.where((post) {
-        if (categoryName.isEmpty) {
-          return true;
-        }
-        final foodEmojis = foodCategory[categoryName] ?? [];
-        return foodEmojis.contains(post.foodTag);
-      }).toList(),
-    );
-  }
-
-  // 全ての投稿を取得
   final query =
       supabase.from('posts').stream(primaryKey: ['id']).order('created_at');
   return query.asyncMap(
     (events) {
-      // ブロックリストでフィルタリング（Mapのまま）
-      final filtered = events.where((post) {
-        return !blockList.contains(post['user_id']);
-      }).toList();
-
-      // 型変換
-      final typed = filtered.map(Posts.fromJson).toList();
-
-      // カテゴリー名が空でない場合は、そのカテゴリーに含まれる全ての絵文字でフィルタリング
+      final mapped = events.map(Posts.fromJson).toList();
+      final filtered =
+          mapped.where((post) => !blockList.contains(post.userId)).toList();
       if (categoryName.isNotEmpty) {
         final foodEmojis = foodCategory[categoryName] ?? [];
-        final result =
-            typed.where((post) => foodEmojis.contains(post.foodTag)).toList();
-        // キャッシュを更新
-        ref.read(postCacheProvider.notifier).state = result;
+        final result = filtered
+            .where((post) => foodEmojis.contains(post.foodTag))
+            .toList();
         return result;
       }
-
-      // キャッシュを更新
-      ref.read(postCacheProvider.notifier).state = typed;
-      return typed;
+      return filtered;
     },
   );
 }
 
+/// 自分の投稿の取得のためのStreamProvider
 @riverpod
 Stream<List<Posts>> myPostStream(Ref ref) {
   final supabase = ref.read(supabaseProvider);
