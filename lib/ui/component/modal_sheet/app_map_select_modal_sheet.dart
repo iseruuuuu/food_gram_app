8ff66a669d404d9ad072c489256a3da905a75a36
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2025-11-16 11:42:16 +0900

    add: 他のマップを選べるようにするためのModalSheetをだしました

---
+++ lib/ui/component/modal_sheet/app_map_select_modal_sheet.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:food_gram_app/gen/l10n/l10n.dart';
+import 'package:go_router/go_router.dart';
+import 'package:map_launcher/map_launcher.dart';
+
+class AppMapSelectModalSheet extends StatelessWidget {
+  const AppMapSelectModalSheet({
+    required this.onMapSelected,
+    super.key,
+  });
