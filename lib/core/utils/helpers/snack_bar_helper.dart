1b3e8d6326822842dc0f42a7bf6d9ea4212e6b96
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2025-12-05 10:58:40 +0900

    fix: Restore toastification dependency and update SnackBar color opacity method

--- lib/core/utils/helpers/snack_bar_helper.dart
+++ lib/core/utils/helpers/snack_bar_helper.dart
@@ -138,7 +138,7 @@ class SnackBarHelper {
             borderRadius: BorderRadius.circular(12),
             boxShadow: [
               BoxShadow(
-                color: Colors.black.withOpacity(0.1),
+                color: Colors.black.withValues(alpha: 0.1),
                 blurRadius: 10,
                 offset: const Offset(0, 4),
               ),
