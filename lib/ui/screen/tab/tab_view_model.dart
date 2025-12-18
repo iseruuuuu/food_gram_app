994dfbe913f3dbb87634dcdf8bd9c3bb7dbd2d77
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2025-12-18 09:27:37 +0900

    add: タブ画面を検索画面からマイマップ画面に変更してみました

--- lib/ui/screen/tab/tab_view_model.dart
+++ lib/ui/screen/tab/tab_view_model.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:food_gram_app/ui/screen/map/map_screen.dart';
+import 'package:food_gram_app/ui/screen/map/my_map/my_map_screen.dart';
 import 'package:food_gram_app/ui/screen/profile/my_profile/my_profile_screen.dart';
-import 'package:food_gram_app/ui/screen/search/search_screen.dart';
 import 'package:food_gram_app/ui/screen/setting/setting_screen.dart';
 import 'package:food_gram_app/ui/screen/tab/tab_state.dart';
 import 'package:food_gram_app/ui/screen/time_line/time_line_screen.dart';
@@ -21,7 +21,7 @@ class TabViewModel extends _$TabViewModel {
   List<Widget> pageList = [
     const MapScreen(),
     const TimeLineScreen(),
-    const SearchScreen(),
+    const MyMapScreen(),
     const MyProfileScreen(),
     const SettingScreen(),
   ];
