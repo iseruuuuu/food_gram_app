0a155054921a2e65036543d5a078d7400ca9a20a
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2025-12-30 15:11:33 +0900

    fix: Remove duplicate import of localization package in app_maybe_not_food_dialog.dart

--- lib/ui/component/dialog/app_maybe_not_food_dialog.dart
+++ lib/ui/component/dialog/app_maybe_not_food_dialog.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:food_gram_app/gen/l10n/l10n.dart';
 import 'package:quickalert/models/quickalert_type.dart';
 import 'package:quickalert/widgets/quickalert_dialog.dart';
-import 'package:food_gram_app/gen/l10n/l10n.dart';
 
 Future<void> showMaybeNotFoodDialog({
   required BuildContext context,
