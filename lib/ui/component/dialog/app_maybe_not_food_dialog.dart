3743231cd4568726e8102d01dd33d151c7e8ccae
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2025-12-30 15:04:04 +0900

    add: 食べ物の可能性ではなかった場合に、警告のダイアログを出現させるようにしました

--- lib/ui/component/dialog/app_maybe_not_food_dialog.dart
+++ lib/ui/component/dialog/app_maybe_not_food_dialog.dart
@@ -1,4 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:quickalert/models/quickalert_type.dart';
+import 'package:quickalert/widgets/quickalert_dialog.dart';
+
+Future<void> showMaybeNotFoodDialog({
+  required BuildContext context,
+  required VoidCallback onContinue,
+  required VoidCallback onDelete,
   String title = 'ちょっと確認 🍽️',
   String text = 'この写真、\n食べものじゃないかも…？と\nFoodGramが首をかしげています 🤔\n\nそれでも投稿しますか？',
+}) async {
+  await QuickAlert.show(
+    context: context,
+    type: QuickAlertType.warning,
+    title: title,
+    text: text,
     confirmBtnText: '続ける',
+    confirmBtnColor: Colors.black,
+    onConfirmBtnTap: () {
+      Navigator.of(context).pop();
+      onContinue();
+    },
+    showCancelBtn: true,
     cancelBtnText: '画像を削除',
+    onCancelBtnTap: () {
+      Navigator.of(context).pop();
+      onDelete();
+    },
+    confirmBtnTextStyle: const TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.bold,
+      color: Colors.white,
+    ),
+    cancelBtnTextStyle: const TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.bold,
+    ),
+  );
+}
