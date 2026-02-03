a47c7fe940c70cd55e97a189d230d498d925ad5f
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2026-02-03 14:24:18 +0900

    add: ズーム具合を16に統一しました

--- lib/ui/screen/map/map_view_model.dart
+++ lib/ui/screen/map/map_view_model.dart
@@ -158,7 +158,7 @@ class MapViewModel extends _$MapViewModel {
               .getRestaurantPosts(lat: latLng.latitude, lng: latLng.longitude);
           restaurant.whenOrNull(success: (posts) => onPinTap(posts));
           await state.mapController?.animateCamera(
-            CameraUpdate.newLatLngZoom(latLng, 14),
+            CameraUpdate.newLatLngZoom(latLng, 16),
             duration: const Duration(seconds: 1),
           );
           state = state.copyWith(isLoading: false, hasError: false);
