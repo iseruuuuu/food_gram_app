f3351e2b892da7b857c1f71effdfa09d939bbdce
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2026-02-22 08:59:30 +0900

    add: マップについての定数を追加しました

---
+++ lib/core/config/constants/map_overlay_constants.dart
@@ -0,0 +1,21 @@
+/// マップオーバーレイ（ピン・ヒートマップ）の定数
+class MapOverlayConstants {
+  MapOverlayConstants._();
+
+  // ランタイムスタイル（SymbolLayer）
+  static const String runtimeSourceId = 'fg_posts_source';
+  static const String runtimeLayerId = 'fg_posts_layer';
+
+  // ヒートマップ
+  static const String heatmapSourceId = 'fg_heatmap_source';
+  static const String heatmapLayerId = 'fg_heatmap_layer';
+
+  /// ズームこの値以下でヒートマップ表示
+  static const double heatmapZoomThreshold = 10;
+
+  /// ズームこの値以下で小さな赤ドット表示
+  static const double smallDotZoomThreshold = 13;
+
+  /// 小さな赤ドットの iconSize（マップ上）
+  static const double smallRedDotIconSize = 0.39;
+}
