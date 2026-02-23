92d22bc1d0224619720594f4fd3dd46e8d8f37d3
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2026-02-23 18:01:43 +0900

    remove: Remove unused Riverpod import from app_map_restaurant_modal_sheet.dart

--- lib/ui/component/modal_sheet/app_map_restaurant_modal_sheet.dart
+++ lib/ui/component/modal_sheet/app_map_restaurant_modal_sheet.dart
@@ -2,7 +2,6 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:easy_debounce/easy_debounce.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:food_gram_app/core/model/model.dart';
 import 'package:food_gram_app/core/model/posts.dart';
 import 'package:food_gram_app/core/supabase/current_user_provider.dart';
