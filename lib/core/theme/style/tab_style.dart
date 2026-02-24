e9e267dbae9fba909c14c0b230e7d04f32ed20ac
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2026-02-24 13:45:55 +0900

    add: ライトモードにおいては、選択していないタブの色を灰色にしました

--- lib/core/theme/style/tab_style.dart
+++ lib/core/theme/style/tab_style.dart
@@ -3,14 +3,13 @@ import 'package:flutter/material.dart';
 class TabStyle {
   TabStyle._();
 
-  /// タブ（アイコン・文言）の色。ライトモードでは選択時は黒、未選択は onSurfaceVariant。ダークモードでは選択/未選択とも白で統一。
+  /// タブ（アイコン・文言）の色。ライトモードでは選択時は黒、未選択は灰色。ダークモードでは選択/未選択とも白で統一。
   static Color tabColor(BuildContext context, {required bool selected}) {
-    final scheme = Theme.of(context).colorScheme;
     final isDark = Theme.of(context).brightness == Brightness.dark;
     if (isDark) {
       return Colors.white;
     }
-    return selected ? Colors.black : scheme.onSurfaceVariant;
+    return selected ? Colors.black : Colors.grey;
   }
 
   static TextStyle tab(BuildContext context, {required bool value}) {
