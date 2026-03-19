9b205830b7c9a4e1365607e9e0be576fd3509695
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2026-03-19 13:04:09 +0900

    add: 投稿詳細画面のRouterの判定をより安全にしました

--- lib/router/go_router_extension.dart
+++ lib/router/go_router_extension.dart
@@ -2,24 +2,23 @@ import 'package:food_gram_app/router/router.dart';
 import 'package:go_router/go_router.dart';
 
 extension GoRouterExtension on GoRouter {
-  String isCurrentLocation() {
+  String? isCurrentLocation() {
     final lastMatch = routerDelegate.currentConfiguration.last;
     final matchList = lastMatch is ImperativeRouteMatch
         ? lastMatch.matches
         : routerDelegate.currentConfiguration;
-    final location = matchList.uri.toString();
-    return currentRoute(location);
+    final leafMatch = matchList.last;
+    final route = leafMatch.route;
+    final currentRouteName = route is GoRoute ? route.name : null;
+    switch (currentRouteName) {
+      case RouterPath.timeLineDetail:
+        return RouterPath.timeLineDetailPost;
+      case RouterPath.myProfileDetail:
+        return RouterPath.myProfileDetailPost;
+      case RouterPath.mapDetail:
+        return RouterPath.mapDetailPost;
+      default:
+        return null;
+    }
   }
-}
-
-String currentRoute(String path) {
-  switch (path) {
-    case '/tab/time_line/time_line/time_line_detail':
-      return RouterPath.timeLineDetailPost;
-    case '/tab/my_profile/my_profile/my_profile_detail':
-      return RouterPath.myProfileDetailPost;
-    case '/tab/map/map/map_detail':
-      return RouterPath.mapDetailPost;
-  }
-  return '';
 }
