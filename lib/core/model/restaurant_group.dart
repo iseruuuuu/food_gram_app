e1e581507bea3f9232dcc3008308398a17e2976c
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2026-03-26 14:23:40 +0900

    add: レストラングループモデルにレストラン情報を追加し、マップ選択に関連するプロパティを追加しました

--- lib/core/model/restaurant_group.dart
+++ lib/core/model/restaurant_group.dart
@@ -1,4 +1,5 @@
 import 'package:food_gram_app/core/model/posts.dart';
+import 'package:food_gram_app/core/model/restaurant.dart';
 
 /// 近隣モーダル用：レストラン名単位にまとめたグループ
 class RestaurantGroup {
@@ -21,8 +22,12 @@ class MapModalSelection {
     required this.name,
     required this.lat,
     required this.lng,
+    this.placeSearchRestaurant,
   });
   final String name;
   final double lat;
   final double lng;
+
+  /// マップ上の地名検索で選んだ店。ヘッダーに住所を出すほか、投稿0件時の投稿導線に使う。
+  final Restaurant? placeSearchRestaurant;
 }
