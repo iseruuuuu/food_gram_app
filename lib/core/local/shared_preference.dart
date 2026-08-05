353487a60ead674061297dc4cb2b5a9423eed082
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2026-08-05 11:01:55 +0900

    add: 初回起動かどうかを判定する内部ストレージを記述

--- lib/core/local/shared_preference.dart
+++ lib/core/local/shared_preference.dart
@@ -28,6 +28,8 @@ enum PreferenceKey {
   lastWeeklySummaryWeekStart,
   lastMonthlySummaryMonthStart,
   wantToGoList,
+  hasSeenFirstPostGuide,
+  hasSeenFirstPostSuccessGuide,
 }
 
 class Preference {
