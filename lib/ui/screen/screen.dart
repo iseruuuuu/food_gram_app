5af74038b10be12220b5a9cbb3ea2469b6183aeb
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2026-09-01 17:34:43 +0900

    add: add screen

--- lib/ui/screen/screen.dart
+++ lib/ui/screen/screen.dart
@@ -1,6 +1,7 @@
 export 'package:food_gram_app/ui/screen/authentication/authentication_screen.dart';
 export 'package:food_gram_app/ui/screen/edit/edit_screen.dart';
 export 'package:food_gram_app/ui/screen/edit_post/edit_post_screen.dart';
+export 'package:food_gram_app/ui/screen/friend/friend_add_screen.dart';
 export 'package:food_gram_app/ui/screen/image_editor/image_editor_screen.dart';
 export 'package:food_gram_app/ui/screen/map/map_screen.dart';
 export 'package:food_gram_app/ui/screen/memory_album/memory_album_detail_screen.dart';
