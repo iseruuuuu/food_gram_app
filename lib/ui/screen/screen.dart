0b3dac410fa5be8310a44dd4ba1197ac597b9099
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2026-09-11 21:14:59 +0900

    fix: 使っていない画面を削除しました

--- lib/ui/screen/screen.dart
+++ lib/ui/screen/screen.dart
@@ -15,7 +15,6 @@ export 'package:food_gram_app/ui/screen/profile/my_profile/my_profile_screen.dar
 export 'package:food_gram_app/ui/screen/profile/my_profile/stored_post_screen.dart';
 export 'package:food_gram_app/ui/screen/profile/my_profile/want_to_go_list_screen.dart';
 export 'package:food_gram_app/ui/screen/profile/user_profile/user_profile_screen.dart';
-export 'package:food_gram_app/ui/screen/record/record_yearly_posts_screen.dart';
 export 'package:food_gram_app/ui/screen/restaurant/restaurant_map_screen.dart';
 export 'package:food_gram_app/ui/screen/restaurant/restaurant_screen.dart';
 export 'package:food_gram_app/ui/screen/restaurant_review/restaurant_review_screen.dart';
